ad3516a1e1b4e9567a393f4b4635caefe4468fd5
Daniil Ryzhkov <daniil.ryzhkov.97@gmail.com>
2024-03-23 13:37:30 +0100

    Sketching up json deserialization technique

---
+++ src/models/lambda_payloads/s3.cpp
@@ -0,0 +1,10 @@
+#include <memory>
+
+#include <aws/core/utils/json/JsonSerializer.h>
+
+#include "models/lambda_payloads/s3.hpp"
+
+using namespace aws_lambda_cpp::models::lambda_payloads;
+using namespace Aws::Utils::Json;
+
+
